4a7d0d82c46a185f79f38389b80987c4b70a3942
David Jasek <davijasek@gmail.com>
2021-09-27 21:52:31 +0100

    base map setup

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,19 +1,18 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Map />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Map from "./components/Map.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Map,
+  },
+};
 </script>
 
 <style>
@@ -24,5 +23,12 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+#app,
+body,
+html {
+  width: 100%;
+  height: 100%;
+  margin: 0;
 }
 </style>
---
+++ client/src/components/Map.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="map" />
+</template>
+
+<script>
+export default {
+  name: "Map",
+  mounted() {
+    google.charts.load("current", {
+      packages: ["geochart"],
+    });
+    google.charts.setOnLoadCallback(drawRegionsMap);
+    function drawRegionsMap() {
+      var data = google.visualization.arrayToDataTable([
+        ["Country", "Popularity"],
+        ["Germany", 200],
+        ["United States", 300],
+        ["Brazil", 400],
+        ["Canada", 500],
+        ["France", 600],
+        ["RU", 700],
+      ]);
+
+      var options = {};
+      var chart = new google.visualization.GeoChart(
+        document.getElementById("map")
+      );
+      chart.draw(data, options);
+    }
+  },
+};
+</script>
+
+<style scoped>
+#map {
+  width: 100%;
+  height: 100%;
+}
+</style>
