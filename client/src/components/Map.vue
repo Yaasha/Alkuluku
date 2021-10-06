4977dc3c5dc1101aed6b67b9d2fd2ab7a7a1aa65
David Jasek <davijasek@gmail.com>
2021-10-06 02:35:29 +0100

    added component library and changed map library

--- client/src/components/Map.vue
+++ client/src/components/Map.vue
@@ -3,30 +3,57 @@
 </template>
 
 <script>
+import * as am4core from "@amcharts/amcharts4/core";
+import * as am4maps from "@amcharts/amcharts4/maps";
+import am4geodata_worldLow from "@amcharts/amcharts4-geodata/worldLow";
+import am4themes_spiritedaway from "@amcharts/amcharts4/themes/spiritedaway";
+import am4themes_animated from "@amcharts/amcharts4/themes/animated";
+
 export default {
   name: "Map",
   mounted() {
-    google.charts.load("current", {
-      packages: ["geochart"],
-    });
-    google.charts.setOnLoadCallback(drawRegionsMap);
-    function drawRegionsMap() {
-      var data = google.visualization.arrayToDataTable([
-        ["Country", "Popularity"],
-        ["Germany", 200],
-        ["United States", 300],
-        ["Brazil", 400],
-        ["Canada", 500],
-        ["France", 600],
-        ["RU", 700],
-      ]);
+    // Themes begin
+    am4core.useTheme(am4themes_spiritedaway);
+    am4core.useTheme(am4themes_animated);
+    // Themes end
 
-      var options = {};
-      var chart = new google.visualization.GeoChart(
-        document.getElementById("map")
-      );
-      chart.draw(data, options);
-    }
+    // Create map instance
+    let chart = am4core.create("map", am4maps.MapChart);
+
+    // Set map definition
+    chart.geodata = am4geodata_worldLow;
+
+    // Set projection
+    chart.projection = new am4maps.projections.Miller();
+    chart.panBehavior = 'none'
+
+    // Create map polygon series
+    let polygonSeries = chart.series.push(new am4maps.MapPolygonSeries());
+    polygonSeries.mapPolygons.template.strokeWidth = 0.5;
+
+    // Exclude Antartica
+    polygonSeries.exclude = ["AQ"];
+
+    // Make map load polygon (like country names) data from GeoJSON
+    polygonSeries.useGeodata = true;
+
+    // Configure series
+    let polygonTemplate = polygonSeries.mapPolygons.template;
+    polygonTemplate.tooltipText = "{name}";
+    polygonTemplate.fill = chart.colors.getIndex(0);
+
+    // Create hover state and set alternative fill color
+    let hs = polygonTemplate.states.create("hover");
+    hs.properties.fill = chart.colors.getIndex(2);
+
+    // Create active state
+    let activeState = polygonTemplate.states.create("active");
+    activeState.properties.fill = chart.colors.getIndex(4);
+
+    // Create an event to toggle "active" state
+    polygonTemplate.events.on("hit", function(ev) {
+      ev.target.isActive = !ev.target.isActive;
+    })
   },
 };
 </script>
