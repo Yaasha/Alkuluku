ba1b80ec80356b036121b9627c052143e093e259
David Jasek <davijasek@gmail.com>
2021-12-13 00:37:32 +0100

    fullscreen pwa

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  devtool: "source-map",
   module: {
     rules: [
       {
@@ -18,6 +19,28 @@ module.exports = {
           },
         ],
       },
+      {
+        test: /\.js$/,
+        include: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+            plugins: ["@babel/plugin-syntax-dynamic-import"],
+          },
+        },
+      },
+      {
+        test: /.js$/,
+        use: ["source-map-loader"],
+        enforce: "pre",
+      },
     ],
+  },
+  externals: function (context, request, callback) {
+    if (/xlsx|canvg|pdfmake/.test(request)) {
+      return callback(null, "commonjs " + request);
+    }
+    callback();
   },
 };
